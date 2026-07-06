b7d4d643734544187331155fc797bdab3ffd8f06
Zhanna M <zhanna@Zhannas-MacBook-Air-2.local>
2026-07-06 22:54:25 +0500

    Fix capacitor config merge

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,13 @@
-const config = {
-  appId: "com.coachspace.app",
-  appName: "Coach Space",
-  webDir: "dist",
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sharapieva.coachspace',
+  appName: 'CoachSpace',
+  webDir: 'dist',
   ios: {
     scrollEnabled: true,
     allowsLinkPreview: false,
-  },
+  }
 };
 
 export default config;
